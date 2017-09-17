22aa0b8ebc5538cd27b16ab53814efe3535c2ac9
JeffMinsungKim <ziznzkak@gmail.com>
2017-09-17 18:08:07 +0900

    first commit

---
+++ src/datelikeus/static/js/project.js
@@ -0,0 +1,61 @@
+/* Project specific Javascript goes here. */
+;(function () {
+
+	
+// Set the date we're counting down to
+	var countDownDate = new Date("Nov 28, 2017 15:37:25").getTime();
+
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+
+	// Get todays date and time
+	var now = new Date().getTime();
+
+	// Find the distance between now an the count down date
+	var distance = countDownDate - now;
+
+	// Time calculations for days, hours, minutes and seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	// Display the result in an element with id="demo"
+	// document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
+	// + minutes + "Minutes " + seconds + "Seconds ";
+
+	// Display the result in an element with id="demo"
+	document.getElementById("days").innerHTML = days +" <small>days</small>";
+	document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
+	document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
+	document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
+
+	// If the count down is finished, write some text
+	if (distance < 0) {
+	 clearInterval(x);
+	 document.getElementById("demo").innerHTML = "The Wedding Ceremony is Over";
+	}
+	}, 1000);
+
+
+
+}());
+/*
+Formatting hack to get around crispy-forms unfortunate hardcoding
+in helpers.FormHelper:
+
+    if template_pack == 'bootstrap4':
+        grid_colum_matcher = re.compile('\w*col-(xs|sm|md|lg|xl)-\d+\w*')
+        using_grid_layout = (grid_colum_matcher.match(self.label_class) or
+                             grid_colum_matcher.match(self.field_class))
+        if using_grid_layout:
+            items['using_grid_layout'] = True
+
+Issues with the above approach:
+
+1. Fragile: Assumes Bootstrap 4's API doesn't change (it does)
+2. Unforgiving: Doesn't allow for any variation in template design
+3. Really Unforgiving: No way to override this behavior
+4. Undocumented: No mention in the documentation, or it's too hard for me to find
+*/
+$('.form-group').removeClass('row');
