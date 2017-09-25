aca2c3b26cb6b23e0a22df4759b5d25c2c528424
JeffMinsungKim <ziznzkak@gmail.com>
2017-09-26 03:01:23 +0900

    Custom redirect has been added

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -279,3 +279,5 @@ ADMIN_URL = r'^admin/'
 
 # Your common stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
+# Custom Redirects
+ACCOUNT_ADAPTER = 'datelikeus.users.adapters.MyAccountAdapter'
